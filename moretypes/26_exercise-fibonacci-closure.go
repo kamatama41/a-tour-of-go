083dd2e4e00c029deaf98b816582075bcbf2eae0
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-04-26 12:00:34 +0900

    Refactor

--- moretypes/26_exercise-fibonacci-closure.go
+++ moretypes/26_exercise-fibonacci-closure.go
@@ -5,11 +5,11 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-	n0, n1 := 1, 0
-	return func() int {
-		newN0, newN1 := n1, n0 + n1
-		n0, n1 = newN0, newN1
-		return n1
+	n0, n1 := 1, 1
+	return func() (res int) {
+		res = n0
+		n0, n1 = n1, n0 + n1
+		return
 	}
 }
 
